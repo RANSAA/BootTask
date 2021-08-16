ca6d16fac5756452a5edd6db9a7c96e38c511b8a
sayaDev <1352892108@qq.com>
2021-08-17 02:43:10 +0800

    no message

--- BootTask/ViewController.m
+++ BootTask/ViewController.m
@@ -7,13 +7,13 @@
 
 #import "ViewController.h"
 
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Do any additional setup after loading the view.
-    [self task];
+    [self taskc];
 
     exit(0);
 }
@@ -26,30 +26,18 @@
 }
 
 
-- (NSDictionary *)getShellTask
+//使用-c, shell命令以为一个合成的字符串
+//shellStr:需要执行shell命令的字符串
+- (void)taskc
 {
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"task" ofType:@"json"];
-    NSData *data = [[NSData alloc] initWithContentsOfFile:path];
-    NSDictionary *task = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-    NSLog(@"task:%@",task);
-    [task writeToFile:@"/Users/kimi/Downloads/12.plist" atomically:YES];
-    return  task;
-}
-
-- (void)task
-{
-//    NSDictionary *task = [self getShellTask];
     NSString *path = [[NSBundle mainBundle] pathForResource:@"task" ofType:@"plist"];
-    NSDictionary *task = [NSDictionary dictionaryWithContentsOfFile:path];
-    NSLog(@"task:%@",task);
-
-
-    NSString *launchPath = task[@"launchPath"];
-    NSArray *arguments = task[@"arguments"];
+    NSDictionary *src = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSString *shellStr = src[@"shell"];
 
     NSTask *certTask = [[NSTask alloc]init];
-    [certTask setLaunchPath:launchPath];
-    [certTask setArguments:arguments];
+    [certTask setLaunchPath:@"/bin/bash"];
+    [certTask setArguments:@[@"-c",shellStr,]];//数组中使用两个item即可
+
     NSPipe *pipe = [NSPipe pipe];
     [certTask setStandardOutput:pipe];
     [certTask setStandardError:pipe];
@@ -59,4 +47,7 @@
     NSString *shellResult = [[NSString alloc] initWithData:[handle readDataToEndOfFile] encoding:NSUTF8StringEncoding];
     NSLog(@"shellResult:%@",shellResult);
 }
+
+
+
 @end
